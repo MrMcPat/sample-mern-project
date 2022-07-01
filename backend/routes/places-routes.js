0a589e2fbd29690c71d0942f1e57f099c8a83cc0
Patrick Liang <pl204511194@yahoo.com>
2022-07-01 09:05:13 -0400

    more places routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -24,4 +24,12 @@ router.get("/:pid", (req, res, next) => {
   res.json({ place });
 });
 
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return userId === p.creator;
+  });
+  res.json({ place });
+});
+
 module.exports = router;
