2d6d6c23552b9c26d0e93961ec43ef2d3db5a5b2
Patrick Liang <pl204511194@yahoo.com>
2022-07-29 21:02:33 -0400

    get users done

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -3,17 +3,18 @@ const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
 
-const DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "Patrick Liang",
-    email: "test@test.com",
-    password: "testers",
-  },
-];
-
-function getUsers(req, res, next) {
-  res.json({ users: DUMMY_USERS });
+async function getUsers(req, res, next) {
+  let users;
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError(
+      "Fetching users failed, please try again later",
+      500
+    );
+    return next(error);
+  }
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 }
 
 async function signup(req, res, next) {
